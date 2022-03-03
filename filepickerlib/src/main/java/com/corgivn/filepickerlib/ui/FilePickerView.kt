3bf6b15db5da1bcc66175fb7e52b13fafdb6e044
Iris <iris.pham@legato.co>
2022-03-03 16:39:34 +0700

    init

---
+++ filepickerlib/src/main/java/com/corgivn/filepickerlib/ui/FilePickerView.kt
@@ -0,0 +1,16 @@
+package com.corgivn.filepickerlib.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.LinearLayoutCompat
+
+class FilePickerView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : LinearLayoutCompat(context, attrs, defStyle) {
+
+    init {
+
+    }
+}
